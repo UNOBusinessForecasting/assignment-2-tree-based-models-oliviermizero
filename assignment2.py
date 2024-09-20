d31707f03bcb9ed3b07847e337a718deb2e2b345
Olivier Niyonshuti Mizero <niyomizer@gmail.com>
2024-09-19 22:57:58 -0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -5,25 +5,22 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv")
+data = pd.read_csv( "https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv" )
 data.head()
 
-# Commented out IPython magic to ensure Python compatibility.
-model = DecisionTreeClassifier( max_depth = 3, min_samples_leaf = 10 )
+model = DecisionTreeClassifier( max_depth = 10, min_samples_leaf = 10 )
 
-Y, X = data["meal"], data.drop(["meal", "id", "DateTime"], axis=1)
+Y, X = data["meal"], data.drop( ["meal", "id", "DateTime"], axis=1 )
 
-x, xt, y, yt = train_test_split(X, Y, test_size=0.33, random_state=42)
+x, xt, y, yt = train_test_split( X, Y, test_size=0.33, random_state=42 )
 
-modelFit = model.fit(x,y)
+modelFit = model.fit( x,y )
 
-print("\n\nIn-sample accuracy: %s%%\n\n"
-#  % str(round(100*accuracy_score(y, model.predict(x)), 2)))
-print("\n\nOut-of-sample accuracy: %s%%\n\n"
-# %str(round(100*accuracy_score(yt, model.predict(xt)), 2)))
+print( "\n\nIn-sample accuracy: %s%%\n\n" % str(round(100*accuracy_score(y, model.predict(x)), 2)) )
+print( "\n\nOut-of-sample accuracy: %s%%\n\n" %str(round(100*accuracy_score(yt, model.predict(xt)), 2)) )
 
-test = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv")
-testNew = test.drop(["meal", "id", "DateTime"], axis=1)
+test = pd.read_csv( "https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv" )
+testNew = test.drop( ["meal", "id", "DateTime"], axis=1 )
 testNew.head()
 
-pred = modelFit.predict(testNew)
+pred = modelFit.predict( testNew )
